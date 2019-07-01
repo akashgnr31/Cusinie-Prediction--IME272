f8855fb1d1d66e402827be6da18e51043502ed7d
Akash Gangwar <37154020+akashgnr31@users.noreply.github.com>
2019-07-01 19:37:51 +0530

    Update and rename Data_Visualisation.r to Data_Preprocessing.r

--- Data_Preprocessing.r
+++ Data_Preprocessing.r
@@ -329,3 +329,125 @@ plotMostImportantIngredientsInCuisine(plot_trainWords,"italian",fillcolorblack)+
 plotMostImportantIngredientsInCuisine(plot_trainWords,"mexican",fillColor2)+ coord_polar("y", start=0)
 plotMostImportantIngredientsInCuisine(plot_trainWords,"thai",fillcolorpink)+ coord_polar("y", start=0)
 plotMostImportantIngredientsInCuisine(plot_trainWords,"moroccan",fillcolororange)+ coord_polar("y", start=0)
+
+# create glove vectors
+
+special_tokenizer <- function(x, ...) space_tokenizer(x, sep = "#", ...)
+it <- itoken(texting, tokenizer = special_tokenizer)
+
+# Glove Word Embedding
+# Create vocabulary, terms will be unigrams
+vocab <- create_vocabulary(it)
+vectorizer <- vocab_vectorizer(vocab)
+
+# use window of 4 for context words
+tcm <- create_tcm(it, vectorizer, skip_grams_window=4L)
+
+# create glove vectors
+glove <- GlobalVectors$new(word_vectors_size=100, vocabulary=vocab, x_max=100)
+invisible(capture.output(wv_matrix <- glove$fit_transform(tcm, n_iter=500)))
+
+cuisine_list <- train3[, unique(cuisine)]
+cuisine_matrix <- wv_matrix[cuisine_list,]
+
+# normalize vectors
+normalize_l2 <- function(x) {x / sqrt(sum(x^2))}
+cuisine_matrix <- t(apply(cuisine_matrix, 1, normalize_l2))
+head(cuisine_matrix, 3)
+
+similarity_matrix <- cuisine_matrix %*% t(cuisine_matrix)
+similarity_matrix <- matrix(ecdf(similarity_matrix)(similarity_matrix),
+                            nrow(similarity_matrix), ncol(similarity_matrix))
+colnames(similarity_matrix) <- rownames(similarity_matrix) <- cuisine_list
+
+options(repr.plot.width=8, repr.plot.height=8)
+pallete=colorRampPalette(c("white","yellow","red","black"),space="rgb") 
+heatmap(similarity_matrix, symm=TRUE,col=pallete(20))
+
+
+library(Rtsne)
+tsne_plot <- function(tsne_matrix, ...) {
+  options(repr.plot.width=8, repr.plot.height=6)
+  x <- tsne_matrix[,1]
+  y <- tsne_matrix[,2]
+  plot(x, y, pch=16, col='blue', cex=0.5, xlim=1.2*range(x), ylim=1.2*range(y))
+  text(x, y, pos=1, ...) 
+}
+set.seed(1)
+cuisine_tsne <- Rtsne(cuisine_matrix, perplexity=2.25, max_iter=1e5)$Y
+tsne_plot(cuisine_tsne, labels=rownames(cuisine_matrix))
+
+#Replacing the " "  by "_"
+train$text <- gsub(" ","_",train$text)
+#Replacing the "#" by " "
+train$text <- gsub("#"," ",train$text) 
+
+#prepreocessing function for Converting into DTM
+prep_fun  = function(x) {
+  stringr::str_replace_all(tolower(x), "[^[:alpha:]]", " ")
+}
+
+  #Taking the word Tokenizer 
+tok_fun = word_tokenizer
+
+#Creating a object to Create a vocabulary for training Data
+it_train = itoken(train$text,
+                  preprocessor = prep_fun,
+                  tokenizer = tok_fun,
+                  ids = train$id,
+                  progressbar = FALSE)
+
+#Creating the object to create vocabulary for test data
+it_test = test$text %>% 
+  prep_fun %>% 
+    tok_fun %>% 
+  itoken(ids = test$id,  progressbar = FALSE)
+
+
+NFOLDS = 4
+
+#Generated the vocab for whole datset
+vocab = create_vocabulary(it_train, ngram = c(1L,2L))
+
+#vocabolary before Trimming
+vocab
+
+#Vocabulary after trimming the Ingredients having more than 50% occurance as well as less than 1% occurance
+vocab = vocab %>% prune_vocabulary(term_count_min = 10, 
+                                   doc_proportion_max = 0.5,
+                                   doc_proportion_min = 0.01,vocab_term_max = 5000)
+
+vocab
+
+trigram_vectorizer = vocab_vectorizer(vocab)
+
+#Generate the Document Term matrix using trimmed Vocab
+dtm_train = create_dtm(it_train, trigram_vectorizer)
+Train_dtm<-as.data.frame(as.matrix(dtm_train))
+Train_dtm<-data.frame(Train_dtm[0:0],cuisine=train$cuisine,Train_dtm[2:ncol(Train_dtm)])
+#Dimension of Finally Obtanined Training_DTM
+dim(Train_dtm)
+tfidf = TfIdf$new(norm = "l2", sublinear_tf = T)
+
+# fit model to train data and transform train data with fitted model
+dtm_train_tfidf = fit_transform(dtm_train, tfidf)
+Train_dtm_tfidf<-as.data.frame(as.matrix(dtm_train_tfidf))
+
+# tfidf modified by fit_transform() call!
+# apply pre-trained tf-idf transformation to test data
+dtm_test_tfidf = create_dtm(it_test, trigram_vectorizer)
+
+dtm_test_tfidf = transform(dtm_test_tfidf, tfidf)
+
+Test_dtm_tfidf<-as.data.frame(as.matrix(dtm_test_tfidf))
+
+glmnet_classifier = cv.glmnet(x = dtm_train_tfidf, y = train[['cuisine']], 
+                              family = 'multinomial', 
+                              alpha = 1,
+                              type.measure = "class",
+                              nfolds = NFOLDS,
+                              thresh = 1e-3,
+                              maxit = 1e3)
+
+
+
