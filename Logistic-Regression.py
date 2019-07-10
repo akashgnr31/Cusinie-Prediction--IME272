b528283acbc1824261432d6376c3716f5aeeccb9
Akash Gangwar <37154020+akashgnr31@users.noreply.github.com>
2019-07-10 10:20:09 +0530

    Update Logistic-Regression.py

--- Logistic-Regression.py
+++ Logistic-Regression.py
@@ -1,1 +1,38 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import pandas as pd
+import json
+import numpy as np
 
+def read_dataset(path):
+    return json.load(open(path)) 
+
+train = read_dataset('../IME672/train.json')
+test = read_dataset('../IME672/test.json')
+
+def generate_text(data):
+    text_data = [" ".join(doc['ingredients']).lower() for doc in data]
+    return text_data 
+    
+train_text = generate_text(train)
+test_text = generate_text(test)
+target = [doc['cuisine'] for doc in train]
+
+tfidf = TfidfVectorizer(binary=True)
+def tfidf_features(txt, flag):
+    if flag == "train":
+        x = tfidf.fit_transform(txt)
+    else:
+        x = tfidf.transform(txt)
+    x = x.astype('float16')
+    return x 
+X = tfidf_features(train_text, flag="train")
+X_test = tfidf_features(test_text, flag="test")
+
+lb = LabelEncoder()
+y = lb.fit_transform(target)
+print ("Train the model ... ")
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2 ,random_state = 0)
